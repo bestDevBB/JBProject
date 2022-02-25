8588ad9e366e6fac8bc77d0714d28f9d94f73155
Boram <cnn93@naver.com>
2022-02-25 17:39:12 +0900

    commit

---
+++ app/src/passport/passport.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+const local = require('./localStrategy.js');
+const User = require('../models/User.js');
+const dao = require('../dao/userDao.js');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => { // 로그인 시 실행
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done) => { // 매 요청 시 실행
+    User.findOne({ where: id })
+    .then(user => done(null, user))
+    .catch(err => done(err));
+  })
+
+  local();
+};
